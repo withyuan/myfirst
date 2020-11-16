eacfd1867a0c67c49bb49512741b13125cc4f804
sulilei <952379088@qq.com>
2020-11-16 20:00:36 +0800

    修改密钥

--- src/main/java/com/edu/untils/AliyunSmsUtils.java
+++ src/main/java/com/edu/untils/AliyunSmsUtils.java
@@ -25,8 +25,8 @@ public class AliyunSmsUtils {
          AccessKey ID
          AccessKeySecret*/
     // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-    static final String accessKeyId = "LTAI4G25pvRhZzDvh8VGQYEP";  // TODO 修改成自己的
-    static final String accessKeySecret = "clZLGbNxjnTujiVc8bZKk9hW6co2lR";   // TODO 修改成自己的
+    static final String accessKeyId = "";  // TODO 修改成自己的
+    static final String accessKeySecret = "";   // TODO 修改成自己的
     public static SendSmsResponse sendSms(String telephone, String code) throws ClientException {
         //可自助调整超时时间
         System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
@@ -42,7 +42,7 @@ public class AliyunSmsUtils {
         //必填:短信签名-可在短信控制台中找到
         request.setSignName("诚购优品电商平台");    // TODO 修改成自己的
         //必填:短信模板-可在短信控制台中找到
-        request.setTemplateCode("SMS_189245750");    // TODO 修改成自己的
+        request.setTemplateCode("");    // TODO 修改成自己的
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
 //        request.setTemplateParam("{\"name\":\"Tom\", \"code\":\"123\"}");
         request.setTemplateParam("{\"code\":\"" + code + "\"}");
